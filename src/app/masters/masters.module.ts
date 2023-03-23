d4c38f25e9ac353117d77b6ce14b0f0357b33331
Rajkumar Sathe <satheraj1400@gmail.com>
2023-03-23 16:27:11 +0530

    changes

--- src/app/masters/masters.module.ts
+++ src/app/masters/masters.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MastersRoutingModule } from './masters-routing.module';
+import { LandingComponent } from './landing.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LandingComponent
+  ],
   imports: [
     CommonModule,
     MastersRoutingModule
