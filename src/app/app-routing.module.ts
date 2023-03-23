7b629c61d658053d08df5c2c2f0843322068cf1a
Sagar <patilsagar1211@gmail.com>
2023-03-23 15:17:46 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "", component : LoginComponent},
+  {path : "footer", component : FooterComponent},
+  {path : 'general', loadChildren:()=>import("./general/general.module").then(m=>m.GeneralModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/shared/api.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http:HttpClient) { }
+
+  baseurl = "http://localhost:8081/";
+
+  post(url:string,data:any){
+    return this.http.post(this.baseurl + url, data)
+  }
+
+  put(url:string,data:any){
+    return this.http.put(this.baseurl + url, data)
+  }
+
+  get(url:string){
+    return this.http.get(url)
+  }
+
+}
