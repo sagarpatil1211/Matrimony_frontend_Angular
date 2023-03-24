7b58f074a9096c9bea81bd5e4604a7ec2bc62d71
Sagar <patilsagar1211@gmail.com>
2023-03-24 13:33:50 +0530

    change

--- src/app/masters/masters.module.ts
+++ src/app/masters/masters.module.ts
@@ -5,12 +5,14 @@ import { MastersRoutingModule } from './masters-routing.module';
 import { LandingComponent } from './landing.component';
 import { BusinessplansComponent } from './businessplans/businessplans.component';
 import { SharedModule } from '../shared/shared.module';
+import { BusinessesComponent } from './businesses/businesses.component';
 
 
 @NgModule({
   declarations: [
     LandingComponent,
-    BusinessplansComponent
+    BusinessplansComponent,
+    BusinessesComponent
   ],
   imports: [
     CommonModule,
